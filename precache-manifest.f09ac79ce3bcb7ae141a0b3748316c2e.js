e4bf65b7a7ad16db60306087629e1e32d185726e
Ade Jordan <adejord@gmail.com>
2021-09-04 14:32:44 +0100

    Updates

---
+++ precache-manifest.f09ac79ce3bcb7ae141a0b3748316c2e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5c0e700001ab7f5de8325fef389d39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a03ead71cddad8266993",
+    "url": "/static/css/main.b68fc530.chunk.css"
+  },
+  {
+    "revision": "c5f286b5474657d3ed04",
+    "url": "/static/js/2.357562b5.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.357562b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a03ead71cddad8266993",
+    "url": "/static/js/main.22a6cf1e.chunk.js"
+  },
+  {
+    "revision": "953920e2a1e45b7fdd02",
+    "url": "/static/js/runtime-main.1dfb9157.js"
+  },
+  {
+    "revision": "466b12c7640203347a8c9b0fb32ed04b",
+    "url": "/static/media/FB.466b12c7.jpg"
+  },
+  {
+    "revision": "822ba48877b2ccff0cc9ace3872a5f1e",
+    "url": "/static/media/kishan.822ba488.jpg"
+  }
+]);
